e74f8a92099560922606e647c8c3961985f88cb9
nVITTOadrianto <nvittoa@live.com>
2024-06-23 23:56:05 +0700

    aplikasi lama dihapus

--- app/src/main/java/com/azhar/recipebox/networking/Api.kt
+++
@@ -1,9 +0,0 @@
-package com.azhar.recipebox.networking
-
-object Api {
-
-    var Categories = "https://www.themealdb.com/api/json/v1/1/categories.php"
-    var Filter = "https://www.themealdb.com/api/json/v1/1/filter.php?c={strCategory}"
-    var DetailRecipe = "https://www.themealdb.com/api/json/v1/1/lookup.php?i={idMeal}"
-
-}
